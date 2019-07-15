419573a48d897548fecc7584e72e5fad53756094
sangdao <sang.dao@newoceaninfosys.com>
2019-07-15 11:50:05 +0700

    AndroidX

--- android/src/main/java/com/taluttasgiran/pickermodule/RNSpinner.java
+++ android/src/main/java/com/taluttasgiran/pickermodule/RNSpinner.java
@@ -3,12 +3,13 @@ package com.taluttasgiran.pickermodule;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.facebook.react.bridge.Callback;
 
--- android/src/main/java/com/taluttasgiran/pickermodule/RNSpinnerAdapter.java
+++ android/src/main/java/com/taluttasgiran/pickermodule/RNSpinnerAdapter.java
@@ -1,11 +1,12 @@
 package com.taluttasgiran.pickermodule;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.facebook.react.bridge.Callback;
 
